aefbe13e3ce9d98239695f3fa305b06e4c37b801
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-07-29 18:53:55 +0100

    chore: wrapping up

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import _ from 'lodash';
-import './style.css';
-
-import Icon from './Nature.jpg';
-
-import Data from './data.xml'
-import Notes from './data.csv'
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
- // Add the image to our existing div.
-
- const myIcon = new Image();
-
- myIcon.src = Icon;
 
 
- element.appendChild(myIcon);
-
- console.log(Data)
- console.log(Notes)
 
 
   return element;
