6a4a57487845e05acfb2c2e3b91d52823e3015b6
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-08-03 19:46:09 +0100

    learn: lazy loading

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import { cube } from "./math";
+import _ from 'lodash'
 
 if (process.env.NODE_ENV !== 'production') console.log('looks like we ar ein development mode!')
 
 
 function component() {
   const element = document.createElement('pre');
+  const button = document.createElement('button')
+  const br = document.createElement('br')
 
+  button.innerHTML = 'Click me and look at the console'
 
-  element.innerHTML = [
-    'Hello webpack!', 
-    '5 cubed is equal to' +  cube(5)
-  ].join('\n\n')
+  element.innerHTML = _.join(['Hello', 'webpack'], '')
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = e => import(/* webpackChunkName: "print" */'./print').then(module => {
+    const print = module.default
+
+    print()
+  })
 
 
 
---
+++ src/print.js
@@ -0,0 +1,6 @@
+console.log('the print.js module has loaded! see the network tab in dev tools...')
+
+
+export default () => {
+    console.log('button Clicked: Here\'s "some text"!')
+}
