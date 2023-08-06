d5d2f19010856da9635fccc65ef525fa4545d410
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-08-06 18:46:38 +0100

    learn: shimming

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+
 
 if (process.env.NODE_ENV !== 'production') console.log('looks like we ar ein development mode!')
 
@@ -10,7 +10,10 @@ function component() {
 
   button.innerHTML = 'Click me and look at the console'
 
-  element.innerHTML = _.join(['Hello', 'webpack'], '')
+  element.innerHTML = join(['Hello', 'webpack'], ' ')
+
+// assume we are in the context of 'window'
+this.alert('Hmmm, this probably isn\'t a great idea')
   element.appendChild(br)
   element.appendChild(button)
 
@@ -27,3 +30,26 @@ function component() {
 
 
 document.body.appendChild(component())
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+
+  .then((response) => response.json())
+
+  .then((json) => {
+
+    console.log(
+
+      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
+
+    );
+
+    console.log(json);
+
+  })
+
+  .catch((error) =>
+
+    console.error('Something went wrong when fetching this data: ', error)
+
+  );
--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,43 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-    }, 
+
+        polyfills: './src/polyfills',
+    
+        index: './src/index.js',
+    
+      },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Production'
+            title: 'Production', 
+            template: 'template.html'
         })
     ], 
     output: {
         filename: '[name].bundle.js', 
         path: path.resolve(__dirname, 'dist'), 
         clean: true
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve('./src/index.js'),
+                use: 'imports-loader?wrapper=window',
+            }, 
+            {
+                test: require.resolve('./src/globals.js'), 
+                use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash',
+            join: ['lodash', 'join']
+        })
+    ]
 }
