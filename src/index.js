2b6174f34582694ed45ed317955df3b0d4489d77
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-08-07 19:01:43 +0100

    chore: clean up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import _ from 'lodash'
 
 if (process.env.NODE_ENV !== 'production') console.log('looks like we ar ein development mode!')
 
@@ -10,10 +10,9 @@ function component() {
 
   button.innerHTML = 'Click me and look at the console'
 
-  element.innerHTML = join(['Hello', 'webpack'], ' ')
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-// assume we are in the context of 'window'
-this.alert('Hmmm, this probably isn\'t a great idea')
+
   element.appendChild(br)
   element.appendChild(button)
 
@@ -32,24 +31,3 @@ this.alert('Hmmm, this probably isn\'t a great idea')
 document.body.appendChild(component())
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-
-  .then((response) => response.json())
-
-  .then((json) => {
-
-    console.log(
-
-      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
-
-    );
-
-    console.log(json);
-
-  })
-
-  .catch((error) =>
-
-    console.error('Something went wrong when fetching this data: ', error)
-
-  );
--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,17 @@
 const path = require('path')
-const webpack = require('webpack')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
     entry: {
 
-        polyfills: './src/polyfills',
-    
         index: './src/index.js',
     
       },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Production', 
-            template: 'template.html'
         })
     ], 
     output: {
@@ -22,22 +19,5 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'), 
         clean: true
     },
-    module: {
-        rules: [
-            {
-                test: require.resolve('./src/index.js'),
-                use: 'imports-loader?wrapper=window',
-            }, 
-            {
-                test: require.resolve('./src/globals.js'), 
-                use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            _: 'lodash',
-            join: ['lodash', 'join']
-        })
-    ]
+
 }
